c6d2b5023f6f43718e47a862ec70559b8b163a18
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2022-01-26 17:21:45 -0300

    Initial Commit

---
+++ TrainingProject/TrainingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingProjectApp.swift
+//  TrainingProject
+//
+//  Created by Guilherme - ília on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TrainingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
